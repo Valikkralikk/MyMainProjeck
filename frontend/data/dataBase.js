19435170b1eda1edafa0c0536e1fc794d6438eaf
Valentin <valikkralikk>
2021-05-11 11:54:55 +0300

    fixed code
    delete return in wrong place

--- frontend/data/dataBase.js
+++ frontend/data/dataBase.js
@@ -9,7 +9,7 @@ const dataBase = [
                 name:'Борис Годунов',
                 date:'1874',
                 notes:[
-                    ['Борис Годунов Клавир','https://disk.yandex.by/i/QEo1nQ4qUOrEQA',true,'../../source/audio/example.mp3'],
+                    ['Борис Годунов Клавир','https://disk.yandex.by/i/QEo1nQ4qUOrEQA',true,'../../source/audio/music.mp3'],
                     ['Борис Годунов Клавир 1896г','https://disk.yandex.by/i/zftuydF7lwfp6g',false,'address'],
                     ['Монолог Бориса Скорбит душа','https://disk.yandex.by/i/f1VKSrH_IJ85vA',false,'address'],
                     ['Монолог Марины','https://disk.yandex.by/i/pfOuPsS1_jDHaQ',false,'address'],
